1573500c46ddaff0be3af3e4d30523b1ea468991
Brutski93 <abrutski@gmail.com>
2025-08-20 22:03:27 +0300

    feat: add more functional

--- js/start-menu.js
+++ js/start-menu.js
@@ -10,6 +10,7 @@ const sectionProfile = document.querySelector('.profile');
 const sectionFight = document.querySelector('.fight');
 const allSections = [sectionStart, sectionMain, sectionSetting, sectionProfile, sectionFight];
 
+const header = document.getElementById('header'); // header
 const buttonMain = document.getElementById('menu-main'); // header
 const buttonSetting = document.getElementById('menu-setting'); // header
 const buttonProfile = document.getElementById('menu-profile'); // header
@@ -32,6 +33,34 @@ const fightNameEnemy = document.querySelector('.fight-name-enemy'); // fight Ene
 const fightImgEnemy = document.querySelector('.fight-img-enemy'); // fight Enemy's image
 const fightHPEnemy = document.querySelector('.fight-hp-enemy'); // fight Enemy's HP
 const fightLog = document.querySelector('.fight-log'); // fight log
+const buttonRunFight = document.querySelector('.run-fight'); // button to attack
+const radioButtons = document.querySelectorAll('.fight-input'); // all inputs in the fight section
+
+const enemys = [{
+  name: 'goblin',
+  ph: 70,
+  attacks: 1,
+  defense: 1
+}, {
+  name: 'orc',
+  ph: 180,
+  attacks: 2,
+  defense: 1
+}, {
+  name: 'dk',
+  ph: 120,
+  attacks: 2,
+  defense: 2
+}];
+
+const zones = [
+'head',
+'body',
+'lefthand',
+'righthand',
+'leftleg',
+'rightleg'
+];
 
 /////////////////////////////////////////////////////////////////////////////////////
 
@@ -43,6 +72,10 @@ buttonSetting.addEventListener('click', showSetting);
 buttonProfile.addEventListener('click', showProfile);
 settingButton.addEventListener('click', showPopupToChangeName);
 settingOk.addEventListener('click', changeNameInSetting);
+buttonRunFight.addEventListener('click', makeAction);
+radioButtons.forEach(a => {
+  a.addEventListener('click', checkIsReadyToFight);
+});
 
 /////////////////////////////////////////////////////////////////////////////////////
 
@@ -59,6 +92,10 @@ function creatAndAppend(whatParent, whatTag, whatClass = false, whatText = false
   return newItem;
 }
 
+function getRandom(max) {
+  return Math.floor(Math.random() * max);
+}
+
 function hideAllSectionsExept(section) {
   allSections.forEach(a => a.classList.add('over-left'));
   section.classList.remove('over-left');
@@ -71,12 +108,15 @@ function showStart() {
 }
 function showSetting() {
   hideAllSectionsExept(sectionSetting);
+  console.log(document.cookie.split(';'));
 }
 function showProfile() {
   hideAllSectionsExept(sectionProfile);
 }
 function showFight() {
   hideAllSectionsExept(sectionFight);
+  if (isFightNotStarted()) getRandonEnemy();
+  else resetFight();
 }
 /* start */
 function changeHero(event) {
@@ -88,6 +128,7 @@ function changeHero(event) {
   divImg.classList.remove('fighter');
   divImg.classList.add(newClass);
   profileImg.classList.add(newClass);
+  fightImg.classList.add(newClass);
   document.cookie = `class=${newClass}`;
 }
 
@@ -103,16 +144,20 @@ function changeName(name) {
   profileName.innerText = name;
   fightName.innerText = name;
 }
+
 function changeNameInSetting() {
   hidePopupToChangeName();
   changeName(input2.value);
 }
+
 function showPopupToChangeName() {
   settinfPopup.classList.remove('hidden');
 }
+
 function hidePopupToChangeName() {
   settinfPopup.classList.add('hidden');
 }
+
 /* profile */
 function changeClass(event) {
   let text = event.target.innerText;
@@ -126,11 +171,138 @@ function changeClass(event) {
   fightImg.classList.add(text);
   document.cookie = `class=${text}`;
 }
+/* fight */
+
+function getCurrentEnemyIndex() {
+  let enemyIndex = false;
+  document.cookie.split(';').forEach(a => {
+    if (a.includes('enemyName')) {
+      enemyIndex = a.slice(11);
+      for (let i = 0; i < enemys.length; i += 1) {
+        if (enemys[i].name === enemyIndex) {
+          enemyIndex = i;
+          break;
+        }
+      }
+    }
+  });
+  return enemyIndex;
+}
+
+function isFightNotStarted() {
+  let fighIsStarted = true;
+  tempDate.forEach(a => {
+    if (a.includes('enemy')) fighIsStarted = false;
+  });
+  return fighIsStarted;
+}
+
+function getRandonEnemy() {
+  const random = getRandom(enemys.length);
+  document.cookie = `enemyName=${enemys[random].name}`;
+  fightNameEnemy.innerText = enemys[random].name[0].toUpperCase() + enemys[random].name.slice(1).toUpperCase();
+  fightImgEnemy.classList.add(`${enemys[random].name}`);
+  fightHPEnemy.style.width = '100%';
+  fightHP.style.width = '100%';
+  document.cookie = `enemyHP=100%`;
+  document.cookie = `playerHP=100%`;
+}
+
+function resetFight() {
+  const allCookie = document.cookie.split(';');
+  allCookie.forEach(a => {
+    if (a.includes('enemyName')) {
+      fightNameEnemy.innerText = a.slice(11, 12).toUpperCase() + a.slice(12);
+      resetEnemyImg(a.slice(11));
+    }
+    if (a.includes('enemyHP')) fightHPEnemy.style.width = a.slice(9);
+    if (a.includes('playerHP')) fightHP.style.width = a.slice(10);
+  });
+}
+
+function resetEnemyImg(name) {
+  for (let i = 0; i < enemys.length; i += 1) {
+    fightImgEnemy.classList.remove(enemys[i].name);
+  }
+  fightImgEnemy.classList.add(name);
+}
+
+function checkIsReadyToFight() {
+  const selectedAttack = document.querySelectorAll('input[name="attack"]:checked');
+  if (selectedAttack.length !== 1) return false;
+  const selectedDefense = document.querySelectorAll('input[name="defense"]:checked');
+  if (selectedDefense.length !== 2) return false;
+  buttonRunFight.classList.add('run-fight-active');
+  return true;
+}
+
+function getArrWithEnemyActions(num) {
+  const arr = [];
+  for (let i = 0; i < num; i += 1) {
+    arr.push(zones[getRandom(zones.length)]);
+  }
+  return arr;
+}
+
+function getPlayerAttack() {
+  const selectedOption = document.querySelector('input[name="attack"]:checked');
+  return selectedOption.value;
+}
+
+function getArrWithPlayerDefense() {
+  const selectedOption = document.querySelectorAll('input[name="defense"]:checked');
+  return [selectedOption[0].value, selectedOption[1].value];
+}
+
+function makeAction() { // main game mechanic !!!
+ if (!buttonRunFight.classList.value.includes('active')) return;
+ // enemy actions
+ const enemyData = enemys[getCurrentEnemyIndex()];
+ const enemyIsAttack = getArrWithEnemyActions(enemyData.attacks);
+ const enemyIsDefense = getArrWithEnemyActions(enemyData.defense);
+ // player actions
+ const playerIsAttack = getPlayerAttack();
+ const playerIsDefense = getArrWithPlayerDefense();
+ campareAttaksAndMakeLog(playerIsAttack, playerIsDefense, enemyIsAttack, enemyIsDefense);
+ clearInputs();
+}
+
+function campareAttaksAndMakeLog(pa, pd, ea, ed) {
+  const playerName = fightName.innerText;
+  const enemyName = fightNameEnemy.innerText;
+  let text = '';
+  if (ed.includes(pa)) {
+    text = `${playerName} attacks ${enemyName}'s ${pa} , but ${enemyName} blocks it.`
+    creatAndAppend(fightLog, 'p', false, text);
+  } else {
+    text = `${playerName} attacks ${enemyName}'s ${pa} and deal to ${enemyName} 10 damage.`;
+    creatAndAppend(fightLog, 'p', false, text);
+  }
+  for (let i = 0; i < ea.length; i += 1) {
+    if (pd.includes(ea[i])) {
+      text = `${enemyName} attacks ${playerName}'s ${ea[i]}, but ${playerName} blocks it.`
+      creatAndAppend(fightLog, 'p', false, text);
+    } else {
+      text = `${enemyName} attacks ${playerName}'s ${ea[i]} and deal to ${playerName} 10 damage.`;
+      creatAndAppend(fightLog, 'p', false, text);
+    }
+  }
+}
+
+function clearInputs() {
+  radioButtons.forEach(a => {
+    a.checked = false;
+  });
+  buttonRunFight.classList.remove('run-fight-active');
+}
 // ------------------- builder --------------------------------------
 let gameInfo = false;
 const tempDate = document.cookie.split(';');
 tempDate.forEach(a => {
-  if (a.includes('gamestarted')) gameInfo = a; // check is character created
+  if (a.includes('gamestarted')) {
+    gameInfo = a; // check is character created
+    header.classList.remove('over-top');
+  }
   if (a.includes('user')) changeName(a.slice(6));
   if (a.includes('class')) {
     profileImg.classList.add(a.slice(7)); // set profile image
